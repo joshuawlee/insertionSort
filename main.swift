4823e06b955fbdc94577ee027b181f5a70c68c65
Joshua Lee <joshua.lee@student.allenisd.org>
2022-10-06 15:13:21 -0500

    initial commit

---
+++ main.swift
@@ -0,0 +1,37 @@
+// Add your code below:
+
+var unsortedArray = [String]()
+while let line = readLine() {
+    unsortedArray.append(line)
+}
+//func lowercase(array: inout [String]) {
+//    for i in 0..<array.count {
+  //      var x = i
+    //    array[x].lowercased()
+      //  x += 1
+    //}
+//}
+
+func swap(array: inout [String], firstIndex: Int, secondIndex: Int) {
+    let swaps = array[firstIndex]
+    array[firstIndex] = array[secondIndex]
+    array[secondIndex] = swaps
+}
+
+func insertionSort(array: inout [String]) {
+    //var stringArray = array
+   
+    for i in 1..<array.count {                    
+        var x = i
+        
+        while x > 0 && array[x] < array[x - 1] {        
+            swap(array: &array, firstIndex: x, secondIndex: x-1)
+            x -= 1            
+        }
+        
+    }
+    print(array)       
+}
+
+
+insertionSort(array: &unsortedArray)
